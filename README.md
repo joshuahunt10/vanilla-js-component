719bab88d540006be57da7002521d06f735399ef
Josh Hunt <j.hunt@cpanel.net>
2020-11-20 08:25:20 -0600

    initial tutorial skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# Purpose
+Explore how to use import and creat components using vanilla Javascript.
+
+Skeleton for the project can be found [here](https://developer.okta.com/blog/2019/12/18/vanilla-javascript-components).
---
+++ js/rater.js
@@ -0,0 +1,40 @@
+export function Rater(ratingElement) {
+    const stars = ratingElement.querySelectorAll('.star');
+
+    const resetRating = ev => {
+        const currentRating = ratingElement.getAttribute('data-rating');
+        highlightRating(currentRating);
+    };
+
+    const highlightRating = (rating) => {
+        stars.forEach(star => {
+          star.style.color =
+            rating >= star.getAttribute('data-value') ? 'yellow' : 'gray';
+        });
+    };
+
+    const ratingHover = ev => {
+        const currentHover = ev.currentTarget.getAttribute('data-value');
+        highlightRating(currentHover);
+    };
+
+    const setRating = ev => {
+        ratingElement.setAttribute(
+          'data-rating',
+          ev.currentTarget.getAttribute('data-value')
+        );
+      };
+
+    resetRating();
+
+    // stars.forEach(star => {
+    //     star.addEventListener('mouseover', ratingHover);
+    // });
+
+    stars.forEach(star => {
+        star.addEventListener('click', setRating);
+        star.addEventListener('mouseover', ratingHover);
+      });
+
+    ratingElement.addEventListener('mouseout', resetRating);
+}
